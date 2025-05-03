7c90b40c4b2d8133c151e2bf39c4dd353782e016
Zhaorui Yang <zhaorui.yang@zju.edu.cn>
2025-05-03 16:41:24 +0800

    native html with numerous bugs and corner cases

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is kit.fontawesome.com cdnjs.cloudflare.com;
+  style-src 'self' 'unsafe-inline' kit.fontawesome.com cdnjs.cloudflare.com;
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
-  font-src 'self';
+  font-src 'self' kit.fontawesome.com cdnjs.cloudflare.com;
   frame-src giscus.app
 `
 
