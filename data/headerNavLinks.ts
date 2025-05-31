09cbe40db32b573a604a22de47a43850feed8b9c
Zhaorui Yang <zhaorui.yang@zju.edu.cn>
2025-05-31 10:56:19 +0800

    add framework in about page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Report' },
+  { href: '/blog', title: 'Reports' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
