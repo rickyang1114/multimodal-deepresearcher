bcf2182469e6569b869099950a0c0c67f4346245
Zhaorui Yang <zhaorui.yang@zju.edu.cn>
2025-07-25 21:27:47 +0800

    Update Main.tsx
    Add authors

--- app/Main.tsx
+++ app/Main.tsx
@@ -34,6 +34,10 @@ export default function Home({ posts }) {
             <sup className="text-blue-600 dark:text-blue-400">§</sup>,
             <span className="font-medium">Xingyu Liu</span>
             <sup className="text-blue-600 dark:text-blue-400">§</sup>,
+            <span className="font-medium">Luoxuan Weng</span>
+            <sup className="text-blue-600 dark:text-blue-400">§</sup>,
+            <span className="font-medium">Yingchaojie Feng</span>
+            <sup className="text-blue-600 dark:text-blue-400">§</sup>,
             <span className="font-medium">Minfeng Zhu</span>
             <sup className="text-orange-600 dark:text-orange-400">‡</sup>
             <sup className="text-green-600 dark:text-green-400">✉</sup>,
